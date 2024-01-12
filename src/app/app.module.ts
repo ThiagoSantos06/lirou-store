1d97a1e189b9674d43b53df33a76b4a46ab2884e
Thiago Santos <thiagohenrique1983c@gmail.com>
2024-01-12 13:09:45 -0300

    Adicionando search bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { Header } from './header/header.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Header
+    Header,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule
