d72029cb316fe780c7a6a0e293517830b4277647
Thiago Santos <thiagohenrique1983c@gmail.com>
2024-01-23 19:50:17 -0300

    criando componente categorias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { Header } from './header/header.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
+import { CategoriasComponent } from './categorias/categorias.component';
 
 @NgModule({
   declarations: [
     Header,
     SearchBarComponent,
     AppComponent,
+    CategoriasComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/categorias/categorias.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categorias',
+  templateUrl: './categorias.component.html',
+  styleUrls: ['./categorias.component.css']
+})
+export class CategoriasComponent {
+  comprinhas = [
+    "assets/Imagem do WhatsApp de 2023-12-15 à(s) 22.58 103.png",
+    "assets/Imagem do WhatsApp de 2023-12-15 à(s) 22.58 103.png",
+    "assets/Imagem do WhatsApp de 2023-12-15 à(s) 22.58 103.png",
+    "assets/Imagem do WhatsApp de 2023-12-15 à(s) 22.58 103.png",
+  ]
+}
