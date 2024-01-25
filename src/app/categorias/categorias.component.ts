316f2ea60b6761de365b66293bafb398e3b2f964
Thiago Santos <thiagohenrique1983c@gmail.com>
2024-01-24 22:26:19 -0300

    adiciona rolagens às categorias

--- src/app/categorias/categorias.component.ts
+++ src/app/categorias/categorias.component.ts
@@ -6,10 +6,111 @@ import { Component } from '@angular/core';
   styleUrls: ['./categorias.component.css']
 })
 export class CategoriasComponent {
-  comprinhas = [
-    "assets/Imagem do WhatsApp de 2023-12-15 à(s) 22.58 103.png",
-    "assets/Imagem do WhatsApp de 2023-12-15 à(s) 22.58 103.png",
-    "assets/Imagem do WhatsApp de 2023-12-15 à(s) 22.58 103.png",
-    "assets/Imagem do WhatsApp de 2023-12-15 à(s) 22.58 103.png",
+  fotoDosOculos = [
+    "https://byrbtssdegflenaymewl.supabase.co/storage/v1/object/public/liroustore/lupinha.png",
+    "https://byrbtssdegflenaymewl.supabase.co/storage/v1/object/public/liroustore/lupinha.png",
+    "https://byrbtssdegflenaymewl.supabase.co/storage/v1/object/public/liroustore/lupinha.png",
+    "https://byrbtssdegflenaymewl.supabase.co/storage/v1/object/public/liroustore/lupinha.png",
+    "https://byrbtssdegflenaymewl.supabase.co/storage/v1/object/public/liroustore/lupinha.png",
+    "https://byrbtssdegflenaymewl.supabase.co/storage/v1/object/public/liroustore/lupinha.png",
+    "https://byrbtssdegflenaymewl.supabase.co/storage/v1/object/public/liroustore/lupinha.png",
+    "https://byrbtssdegflenaymewl.supabase.co/storage/v1/object/public/liroustore/lupinha.png",
+    "https://byrbtssdegflenaymewl.supabase.co/storage/v1/object/public/liroustore/lupinha.png",
+    "https://byrbtssdegflenaymewl.supabase.co/storage/v1/object/public/liroustore/lupinha.png",
+    "https://byrbtssdegflenaymewl.supabase.co/storage/v1/object/public/liroustore/lupinha.png",
+    "https://byrbtssdegflenaymewl.supabase.co/storage/v1/object/public/liroustore/lupinha.png",
+    "https://byrbtssdegflenaymewl.supabase.co/storage/v1/object/public/liroustore/lupinha.png",
+    "https://byrbtssdegflenaymewl.supabase.co/storage/v1/object/public/liroustore/lupinha.png",
+    "https://byrbtssdegflenaymewl.supabase.co/storage/v1/object/public/liroustore/lupinha.png",
   ]
+
+  todosOsOculos = [
+    {
+      nome: "CHEFE LIXO",
+      foto: "https://byrbtssdegflenaymewl.supabase.co/storage/v1/object/public/liroustore/lupinha.png"
+    },
+
+    {
+      nome: "PENNY",
+      foto: "https://byrbtssdegflenaymewl.supabase.co/storage/v1/object/public/liroustore/lupinha.png"
+    },
+
+    {
+      nome: "DART",
+      foto: "https://byrbtssdegflenaymewl.supabase.co/storage/v1/object/public/liroustore/lupinha.png"
+    },
+
+    {
+      nome: "FLAK 10",
+      foto: "https://byrbtssdegflenaymewl.supabase.co/storage/v1/object/public/liroustore/lupinha.png"
+    },
+
+    {
+      nome: "ROMEO 2",
+      foto: "https://byrbtssdegflenaymewl.supabase.co/storage/v1/object/public/liroustore/lupinha.png"
+    },
+
+    {
+      nome: "BRUSHED",
+      foto: "https://byrbtssdegflenaymewl.supabase.co/storage/v1/object/public/liroustore/lupinha.png"
+    },
+
+    {
+      nome: "PENNY 24K",
+      foto: "https://byrbtssdegflenaymewl.supabase.co/storage/v1/object/public/liroustore/lupinha.png"
+    },
+
+    {
+      nome: "JULIET 2.0",
+      foto: "https://byrbtssdegflenaymewl.supabase.co/storage/v1/object/public/liroustore/lupinha.png"
+    },
+
+    {
+      nome: "PICA GRANDE",
+      foto: "https://byrbtssdegflenaymewl.supabase.co/storage/v1/object/public/liroustore/lupinha.png"
+    },
+
+    {
+      nome: "PICA PEQUENA",
+      foto: "https://byrbtssdegflenaymewl.supabase.co/storage/v1/object/public/liroustore/lupinha.png"
+    },
+
+    {
+      nome: "JULIET AMANHÃ",
+      foto: "https://byrbtssdegflenaymewl.supabase.co/storage/v1/object/public/liroustore/lupinha.png"
+    },
+
+    {
+      nome: "PENNYS 2.0",
+      foto: "https://byrbtssdegflenaymewl.supabase.co/storage/v1/object/public/liroustore/lupinha.png"
+    },
+
+    {
+      nome: "ROMEO E JULIET",
+      foto: "https://byrbtssdegflenaymewl.supabase.co/storage/v1/object/public/liroustore/lupinha.png"
+    },
+
+    {
+      nome: "JULIET",
+      foto: "https://byrbtssdegflenaymewl.supabase.co/storage/v1/object/public/liroustore/lupinha.png"
+    },
+
+    {
+      nome: "ROMEO 2",
+      foto: "https://byrbtssdegflenaymewl.supabase.co/storage/v1/object/public/liroustore/lupinha.png"
+    },
+
+    {
+      nome: "ROMEO 2",
+      foto: "https://byrbtssdegflenaymewl.supabase.co/storage/v1/object/public/liroustore/lupinha.png"
+    },
+  ]
+
+  ngOnInit() {
+    document.addEventListener('wheel', function(event) {
+      const delta = Math.sign(event.deltaY);
+      document.querySelector('.container')!.scrollLeft += delta * 50; // ajuste o valor 50 conforme necessário
+     }, false);
+  }
 }
+
