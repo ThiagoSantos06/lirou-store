a0417fb9404defa51bc47d8ee4afd4d882a84d03
Thiago Santos <thiagohenrique1983c@gmail.com>
2024-02-07 22:12:28 -0300

    Corrige tamanho das imagens do site

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -4,14 +4,12 @@ import { HomeComponent } from "./client/pages/home/home.component";
 import { CartComponent } from "./client/pages/cart/cart.component";
 import { RegisterComponent } from "./client/pages/register/register.component";
 import { LoginComponent } from "./client/pages/login/login.component";
-import { ProdutosComponent } from "./client/pages/home/produtos/produtos.component";
 import { PurchaseProductComponent } from "./client/pages/purchase-product/purchase-product.component";
 
 const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'cart', component: CartComponent},
     {path: 'product/:id', component: PurchaseProductComponent},
-    {path: 'product', component: ProdutosComponent},
     {path: 'register', component: RegisterComponent},
     {path: 'login', component: LoginComponent}
 ]
