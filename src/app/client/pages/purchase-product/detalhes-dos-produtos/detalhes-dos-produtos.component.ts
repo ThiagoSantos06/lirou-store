5ff8f517fc9b35e9a2544c6dc5e9357d1c58f1b3
Thiago Santos <thiagohenrique1983c@gmail.com>
2024-03-09 12:18:44 -0300

    Adiciona função de aumentar quantidade do óculos na página de compra

--- src/app/client/pages/purchase-product/detalhes-dos-produtos/detalhes-dos-produtos.component.ts
+++ src/app/client/pages/purchase-product/detalhes-dos-produtos/detalhes-dos-produtos.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { GlassesDTO } from 'src/app/shared/models/GlassesDTO';
 import { GlassesService } from "../../../services/GlassesService";
@@ -10,37 +10,41 @@ import { ShippingService } from 'src/app/client/services/ShippingService';
   templateUrl: './detalhes-dos-produtos.component.html',
   styleUrls: ['./detalhes-dos-produtos.component.css']
 })
-export class DetalhesDosProdutosComponent implements OnInit{
-  @Input() productIdentifier: string = ''
-  glasses!: GlassesDTO
+export class DetalhesDosProdutosComponent implements OnInit {
+  @Input() productIdentifier: string = '';
+  glasses!: GlassesDTO;
 
-  valoresDosFretes: ShippingDTO[] = []
-  inputUsuario: string = ''
+  valoresDosFretes: ShippingDTO[] = [];
+  inputUsuario: string = '';
   opcaoSelecionada: string = 'opcao1';
-  
-  constructor(private router: Router, private glassesService: GlassesService, private shppingServie: ShippingService) {}
+
+  constructor(private router: Router, private glassesService: GlassesService, private shppingServie: ShippingService) { }
+
+  ngOnInit() {
+    this.glassesService.getSingleGlasses(this.productIdentifier).subscribe(glasses => {
+      this.glasses = glasses;
+      // Definindo quantidade mínima como 1
+      if (this.glasses.quantityInStock < 1) {
+        this.glasses.quantityInStock = 1;
+      }
+    });
+
+    this.shppingServie.calculateShipping(this.inputUsuario).subscribe(valoresDosFretes => {
+      this.valoresDosFretes = valoresDosFretes;
+    });
+
+    window.scrollTo(0, 0); // Rola para o topo da página
+    this.calcularOFrete();
+  }
 
   calcularOFrete() {
     this.shppingServie.calculateShipping(this.inputUsuario).subscribe((listaDeFretes: ShippingDTO[]) => {
-      this.valoresDosFretes = listaDeFretes
-    })
-  }
-
-  ngOnInit() {
-    this.glassesService.getSingleGlasses(this.productIdentifier).subscribe(glasses=> {
-      this.glasses = glasses
-    })
-
-    this.shppingServie.calculateShipping(this.inputUsuario).subscribe(valoresDosFretes=> {
-      this.valoresDosFretes = valoresDosFretes
-    })
-    
-    window.scrollTo(0, 0); // Rola para o topo da página
-    this.calcularOFrete()
+      this.valoresDosFretes = listaDeFretes;
+    });
   }
 
   navigateToProductDetails(productId: string) {
-    this.router.navigate(['/product', productId])
+    this.router.navigate(['/product', productId]);
   }
 
   formatarCEP(event: any) {
@@ -55,5 +59,22 @@ export class DetalhesDosProdutosComponent implements OnInit{
     if (this.opcaoSelecionada !== 'nenhum') {
       this.valoresDosFretes = []; // Limpa o array de fretes
     }
+  }
+
+  increaseQuantity() {
+    this.glasses.quantityInStock++;
+    this.glasses.price += this.glasses.price / (this.glasses.quantityInStock - 1);
+  }
+  
+  decreaseQuantity() {
+    if (this.glasses.quantityInStock > 1) {
+      this.glasses.price -= this.glasses.price / this.glasses.quantityInStock;
+      this.glasses.quantityInStock--;
+    }
+  }
+
+  updatePrice() {
+    // Ajuste de preço
+    this.glasses.price = this.glasses.price * this.glasses.quantityInStock;
   }
 }
